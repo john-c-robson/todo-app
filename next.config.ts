d4a10d176722cef2ed5522dbe0f7b9b360ce2bf2
John <john_r92@hotmail.co.uk>
2025-02-23 17:08:07 +0000

    Deploy on push functionality

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,23 @@
-import type { NextConfig } from 'next';
+/** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactStrictMode: true,
-};
+let assetPrefix = ''
+let basePath = ''
 
-export default nextConfig;
+if (isGithubActions) {
+  assetPrefix = `/todo-app/`
+  basePath = `/todo-app`
+}
+
+const nextConfig = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  // images: {
+  //   loader: "imgix",
+  //   path: 'the "domain" of your Imigix source',
+  // },
+  output: 'export',
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
